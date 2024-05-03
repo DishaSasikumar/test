1c3e1621a56151ecc2acf6efd52a53d8dc0464f5
DishaSasikumar <94157186+DishaSasikumar@users.noreply.github.com>
2024-05-03 13:19:58 +0530

    test

---
+++ RatingUI/interactive-rating-component-main/rate.js
@@ -0,0 +1,6 @@
+
+function rating(){
+    var rate=document.getElementById("cir").value
+    var main=document.getElementsByTagName("body").value
+   rate= document.getElementById("main").innerHTML="You Selected "+ rate +" out of 5"
+}
